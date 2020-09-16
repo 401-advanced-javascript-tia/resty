5931e9bf0a61004635bfdd0f15db53335f613801
Tia Low <lowtia@gmail.com>
2020-09-16 06:55:20 -0700

    Passing tests

--- resty/src/__tests__/app.test.js
+++ resty/src/__tests__/app.test.js
@@ -4,21 +4,34 @@ import '@testing-library/jest-dom/extend-expect';
 // import App from '../App.js';
 import Results from '../components/results/results.js';
 
+const person = {
 
-it('should render Star Wars list', () => {
+  'name': 'Luke Skywalker',
+  'height': '172',
+  'mass': '77',
+}
 
-  const person = {
+const header = 'application/json';
 
-    'name': 'Luke Skywalker',
-    'height': '172',
-    'mass': '77',
-  }
 
-  const header = 'application/json';
+it('should render results count', () => {
+  render (<Results count={82} headers={header} results={person} />);
+  const count = screen.getByTestId('count');
+  expect(count).toHaveTextContent('Count: 82');
+})
 
-render (<Results count={82} headers={header} results={person} />);
 
-const count = screen.getByTestId('count');
-expect(count).toHaveTextContent('Count: 82');
+it('should render header', () => {
+  render (<Results count={82} headers={header} results={person} />);
+  const headers = screen.getAllByRole('heading');
+  expect(headers).toHaveLength(2);
+  expect(headers[1]).toHaveTextContent('application/json');
+})
 
+
+it('should render results', () => {
+  render (<Results count={82} headers={header} results={person} />);
+  const results = screen.getByTestId('results');
+  expect(results).toHaveTextContent('Luke Skywalker');
+  expect(results).toHaveTextContent('height');
 })
--- resty/src/components/results/results.js
+++ resty/src/components/results/results.js
@@ -12,9 +12,9 @@ function Results (props) {
   // console.log('RESULTS IN RESULTS.JS', results);
 
   return (<div className="App-results">
-    <h1 data-testid="count">Count: {props.count}</h1>
-    <h1>Headers: {props.headers}</h1>
-    <h1>Response: <JSONPretty data={props.results}></JSONPretty></h1>
+    <h3 data-testid="count">Count: {props.count}</h3>
+    <h3>Headers: {props.headers}</h3>
+    <div data-testid="results">Response: <JSONPretty data={props.results}></JSONPretty></div>
    
     {/* <ul>{
     results.map(result => <li>{JSON.stringify(result)}</li>)
