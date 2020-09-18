3734b24365b57ae124c36845405aad101ceddf44
Tia Low <lowtia@gmail.com>
2020-09-18 07:06:28 -0700

    Installed react router, created menu bar with working links, addtl history page and help components

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './header.scss';
 
 function Header() {
   return (<header className="App-header">
     <h1>ReSTy</h1>
+    <nav>
+      <li>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/history">History</NavLink>
+        <NavLink to="/help">Help</NavLink>
+      </li>
+    </nav>
   </header>);
 }
 
---
+++ src/components/historyPage/history-page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './history-page.scss';
+
+
+function HistoryPage(props) {
+
+  return (
+    <div className="App-history-page">
+      
+
+      <aside>
+        <h2>Search History</h2>
+      </aside>
+
+      <section>
+        {/* do something here to say if results true, show this section */}
+        <h2>Results</h2>
+      </section>
+
+    </div>
+
+  )
+}
+
+
+export default HistoryPage;
